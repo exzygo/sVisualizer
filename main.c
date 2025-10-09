1b7410b1800ca00a996f5854a20a893dabbd4219
lfg <vanproller@gmail.com>
2025-10-09 00:38:39 +0000

    Change GNU GPLv2 License header on main.c

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025 fasmvg
+ * Copyright (C) 2025 letterfenceglasses
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
